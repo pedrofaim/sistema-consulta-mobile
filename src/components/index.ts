752665732ef33a4dfde728ecff6474af12a4ecf8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-30 22:20:11 -0300

    Navegação + Autenticação com Context API

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 /**
- * Exportacao centralizada dos componentes da aplicacao.
+ * Exportação centralizada dos componentes
  */
+
 export { default as ConsultaCard } from "./ConsultaCard";
+export { default as Loading } from "./Loading";
+export { default as EmptyState } from "./EmptyState";
